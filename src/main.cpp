145b9aad800ddf66234b36a7bbed03c2d8009133
Ariel Lima Dallari Guerreiro <arieldallari@usp.br>
2022-06-16 20:15:31 -0300

    testagem de servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include <ESP32Servo.h>
 #include <Arduino.h>
 #include "leds.h"
-#include "motor.h"
+#include "servo.h"
 #include "sensor_ultrassonico.h"
 #include "balanca.h"
 #include <Ultrasonic.h>
 #include "HX711.h"
 
-#define VEL 255
-#define SERVO_PIN 32
+#define SERVO_PIN 21
 
 float dist_CM;
 double peso;
@@ -32,6 +31,8 @@ sensor_ultrassonico sensor_u(PINO_TRIGGER, PINO_ECCHO);
 // Cria objeto balança
 balanca bal(DT_BALANCA, SCK_BALANCA);
 
+servo servoMot(SERVO_PIN);
+
 void setup() {
   // Chama funções de teste
   led_vermelho.blink(200);
@@ -47,11 +48,8 @@ void setup() {
 	ESP32PWM::allocateTimer(1);
 	ESP32PWM::allocateTimer(2);
 	ESP32PWM::allocateTimer(3);
-	myservo.setPeriodHertz(50);    // standard 50 hz servo
-	myservo.attach(SERVO_PIN, 1000, 2000); // attaches the servo on pin 18 to the servo object
-	// using default min/max of 1000us and 2000us
-	// different servos may require different min/max settings
 	// for an accurate 0 to 180 sweep
+  servoMot.sweep();
 
   // Inicializa Porta serial
   Serial.begin(19200);
@@ -74,6 +72,9 @@ void loop() {
   Serial.print("\nPeso: ");
   Serial.print(peso);
   Serial.print("g");
+
+  servoMot.sweep();
+  delay(100);
 //   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
 //     // in steps of 1 degree
 //     myservo.write(pos);              // tell servo to go to position in variable 'pos'
@@ -83,4 +84,6 @@ void loop() {
 //     myservo.write(pos);              // tell servo to go to position in variable 'pos'
 //     delay(7);                       // waits 15ms for the servo to reach the position
 //   }
+
 }
+
